084cda9ea503f1f1281de8188e33836386c05086
Rommel Lasso <soccerply@msn.com>
2017-11-12 02:20:13 -0500

    Added Conversion Functions

---
+++ conversions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Assignemnt Week Six"""
+
+def convertCelsiusToKelvin():
+    """Converts Celcius to Kelvin
+
+    Args:
+        Value (float): Celcius value
+
+    Returns:
+        convertedValue: Kelvin Value
+    """
+
+    convertedValue = 0.0
+
+def convertCelsiusToFahrenheit():
+    """Converts Celcius to Fahrenheit
+
+    Args:
+        Value (float): Celcius value
+
+    Returns:
+        convertedValue: Farenheit Value
+    """
+
+    convertedValue = 0.0
+
